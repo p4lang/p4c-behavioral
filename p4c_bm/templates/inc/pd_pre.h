f28ed4dd3e97b73838719639bc41d416a25db57e
krishna <krishna@barefootnetworks.com>
2015-08-13 11:26:52 -0700

    adding device id to all multicast pd api's

--- p4c_bm/templates/inc/pd_pre.h
+++ p4c_bm/templates/inc/pd_pre.h
@@ -29,18 +29,18 @@ p4_pd_mc_node_create(p4_pd_sess_hdl_t sess_hdl, int dev, uint16_t rid,
                      p4_pd_entry_hdl_t *node_hdl);
 
 p4_pd_status_t
-p4_pd_mc_node_update(p4_pd_sess_hdl_t sess_hdl, p4_pd_entry_hdl_t node_hdl,
+p4_pd_mc_node_update(p4_pd_sess_hdl_t sess_hdl, int dev, p4_pd_entry_hdl_t node_hdl,
                      uint8_t *port_map, uint8_t *lag_map);
 
 p4_pd_status_t
-p4_pd_mc_node_destroy(p4_pd_sess_hdl_t sess_hdl, p4_pd_entry_hdl_t node_hdl);
+p4_pd_mc_node_destroy(p4_pd_sess_hdl_t sess_hdl, int dev, p4_pd_entry_hdl_t node_hdl);
 
 p4_pd_status_t
-p4_pd_mc_associate_node(p4_pd_sess_hdl_t sess_hdl, p4_pd_entry_hdl_t grp_hdl,
+p4_pd_mc_associate_node(p4_pd_sess_hdl_t sess_hdl, int dev, p4_pd_entry_hdl_t grp_hdl,
                         p4_pd_entry_hdl_t l1_hdl);
 
 p4_pd_status_t
-p4_pd_mc_dissociate_node(p4_pd_sess_hdl_t sess_hdl, p4_pd_entry_hdl_t grp_hdl,
+p4_pd_mc_dissociate_node(p4_pd_sess_hdl_t sess_hdl, int dev, p4_pd_entry_hdl_t grp_hdl,
                          p4_pd_entry_hdl_t node_hdl);
 
 p4_pd_status_t
--- p4c_bm/templates/src/pd_pre.c
+++ p4c_bm/templates/src/pd_pre.c
@@ -43,27 +43,27 @@ p4_pd_mc_node_create(p4_pd_sess_hdl_t sess_hdl, int dev, uint16_t rid,
 }
 
 p4_pd_status_t
-p4_pd_mc_node_update(p4_pd_sess_hdl_t sess_hdl, p4_pd_entry_hdl_t node_hdl,
+p4_pd_mc_node_update(p4_pd_sess_hdl_t sess_hdl, int dev, p4_pd_entry_hdl_t node_hdl,
                      uint8_t *port_map, uint8_t *lag_map)
 {
     return mc_node_update(sess_hdl, node_hdl, port_map, lag_map);
 }
 
 p4_pd_status_t
-p4_pd_mc_node_destroy(p4_pd_sess_hdl_t sess_hdl, p4_pd_entry_hdl_t node_hdl)
+p4_pd_mc_node_destroy(p4_pd_sess_hdl_t sess_hdl, int dev, p4_pd_entry_hdl_t node_hdl)
 {
     return mc_node_destroy(sess_hdl, node_hdl);
 }
 
 p4_pd_status_t
-p4_pd_mc_associate_node(p4_pd_sess_hdl_t sess_hdl, p4_pd_entry_hdl_t grp_hdl,
+p4_pd_mc_associate_node(p4_pd_sess_hdl_t sess_hdl, int dev, p4_pd_entry_hdl_t grp_hdl,
                         p4_pd_entry_hdl_t l1_hdl)
 {
     return mc_associate_node(sess_hdl, grp_hdl, l1_hdl);
 }
 
 p4_pd_status_t
-p4_pd_mc_dissociate_node(p4_pd_sess_hdl_t sess_hdl, p4_pd_entry_hdl_t grp_hdl,
+p4_pd_mc_dissociate_node(p4_pd_sess_hdl_t sess_hdl, int dev, p4_pd_entry_hdl_t grp_hdl,
                          p4_pd_entry_hdl_t node_hdl)
 {
     return mc_dissociate_node(sess_hdl, grp_hdl, node_hdl);
